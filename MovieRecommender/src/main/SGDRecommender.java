f21c192492c7771ff1165ee4cc731698da8ebef0
sshihata <samy.s.shihata@gmail.com>
2013-08-05 11:42:20 +0200

    initial commit

---
+++ MovieRecommender/src/main/SGDRecommender.java
@@ -0,0 +1,10 @@
+package main;
+
+import utils.FileParser;
+
+public class SGDRecommender {
+	public static void main(String...args){
+		FileParser fileParser = new FileParser();
+		fileParser.load();
+	}
+}
---
+++ MovieRecommender/src/main/SVMRecommender.java
@@ -0,0 +1,9 @@
+package main;
+
+import jnisvmlight.SVMLightInterface;
+
+public class SVMRecommender {
+	public static void main(String...args){
+		SVMLightInterface trainer = new SVMLightInterface();
+	}
+}
