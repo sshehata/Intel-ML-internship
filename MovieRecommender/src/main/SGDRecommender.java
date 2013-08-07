8a1fd9fe62c5fc26168ef2ac442c02c47d263291
sshihata <samy.s.shihata@gmail.com>
2013-08-07 06:51:07 +0200

    removed redundant classes

--- MovieRecommender/src/main/SGDRecommender.java
+++ MovieRecommender/src/main/SGDRecommender.java
@@ -1,10 +1,7 @@
 package main;
 
-import utils.FileParser;
-
 public class SGDRecommender {
 	public static void main(String...args){
-		FileParser fileParser = new FileParser();
-		fileParser.load();
+
 	}
 }
